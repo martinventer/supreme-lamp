836dedf1db60b43215c347463a1cc3031271c866
martinventer <mpventer@sun.ac.za>
2020-05-19 16:47:35 +0200

    Now able to identify isomorphs.

--- Mesher.py
+++ Mesher.py
@@ -7,6 +7,10 @@ from matplotlib.patches import RegularPolygon
 import networkx as nx
 import itertools
 
+import operator as op
+from functools import reduce
+
+
 def make_hex_grid(dimension) -> dict:
     """
     Create a dictionary of hexagonal grid elements.
@@ -16,90 +20,88 @@ def make_hex_grid(dimension) -> dict:
         {element_ID: [x-coord, y-coord, z-coord], }
     """
     coord_list = []
-    for x in range(-1*dimension, dimension +1, 1):
-        for y in range(-1*dimension, dimension +1, 1):
-            for z in range(-1*dimension, dimension +1, 1):
+    for x in range(-1 * dimension, dimension + 1, 1):
+        for y in range(-1 * dimension, dimension + 1, 1):
+            for z in range(-1 * dimension, dimension + 1, 1):
                 if sum([x, y, z]) is 0:
                     coord_list.append([x, y, z])
 
-    return {id: coord for id, coord in enumerate(coord_list)}
+    return {element_id: coord for element_id, coord in enumerate(coord_list)}
 
 
-def hex_to_cart(hex_coords) -> (list, list):
+def hex_to_cart(hex_coord) -> (list, list):
     """
-    Takes a list of hex coords and converts them to a list of cartesian
+    Takes a list of hex coord and converts them to a list of cartesian
     coordinates.
-    :param hex_coords: list
+    :param hex_coord: list
         list of hex coordinates [[x, y, z], ]
     :return: (list, list)
         pair of coordinate lists x and y ([x], [y])
     """
-    x_coords = [c[0] for c in hex_coords]
-    y_coords = [2. * np.sin(np.radians(60)) * (c[1] - c[2]) / 3. for c in
-                hex_coords]
+    x_coord = [c[0] for c in hex_coord]
+    y_coord = [2. * np.sin(np.radians(60)) * (c[1] - c[2]) / 3. for c in
+               hex_coord]
 
-    return x_coords, y_coords
+    return x_coord, y_coord
 
 
 def plot_hex(elements, active=None) -> None:
     """
-    Plots a hex grid with color and label overlay. Conversion of hexagon to
-    cartesian coordinates.
+    Plots a hex_patch grid with color and label overlay. Conversion of
+    hexagon to cartesian coordinates.
     :param elements: dict
-        dictionary of hex elements
+        dictionary of hex_patch elements
     :param active: dict
         dictionary of active elements, must be same length as elements
     :return: None
     """
     # handle colours
     if not active:
-        color_map = []
         color_map = ["red"] * len(elements)
     else:
         color_map = []
-        for elem, act in active.items():
-            if act:
+        for element in elements.keys():
+            if element in active:
                 color_map.append('blue')
             else:
                 color_map.append('red')
 
-    # create lable and coord  list
-    lables = elements.keys()
-    hex_coords = elements.values()
-
-    # convert from hex [x, y, z] to cart [x, y]
-    x_coords, y_coords = hex_to_cart(hex_coords)
+    # create label and coord  list
+    labels = elements.keys()
+    hex_coord = elements.values()
+    # convert from hex_patch [x, y, z] to cart [x, y]
+    x_coord, y_coord = hex_to_cart(hex_coord)
 
     # process the figure
     fig, ax = plt.subplots(1)
     ax.set_aspect('equal')
 
-    for x, y, c, l in zip(x_coords, y_coords, color_map, lables):
-        hex = RegularPolygon((x, y),
-                             numVertices=6,
-                             radius=2. / 3.,
-                             orientation=np.radians(30),
-                             facecolor=c,
-                             alpha=0.2,
-                             edgecolor='k')
+    for x, y, c, l in zip(x_coord, y_coord, color_map, labels):
+        hex_patch = RegularPolygon((x, y),
+                                   numVertices=6,
+                                   radius=2. / 3.,
+                                   orientation=np.radians(30),
+                                   facecolor=c,
+                                   alpha=0.2,
+                                   edgecolor='k')
 
-        ax.add_patch(hex)
+        ax.add_patch(hex_patch)
         # Also add a text label
         ax.text(x, y + 0.2, l, ha='center', va='center', size=20)
 
     # Also add scatter points in hexagon centres
-    ax.scatter(x_coords, y_coords, c=color_map, alpha=0.5)
-
+    ax.scatter(x_coord, y_coord, c=color_map, alpha=0.5)
     plt.show()
-
     return None
 
 
-def active_subset_random(grid, bias) -> dict:
+def active_subset_random(grid, bias, depth=3) -> list:
     """
     Creates a dictionary of random active assignments.
+    :param depth: int
+        the number of active elements to return
     :param grid: dict
-        hex elemenet dictionary
+        hex element dictionary
     :param bias: float
         a value between 0.0 and 1.0 indicating the fraction of grid elements
         that should be active
@@ -107,11 +109,7 @@ def active_subset_random(grid, bias) -> dict:
         a dictionary containing the element id and a boolean indicator of
         activity
     """
-    return {
-        ids: active for ids, active in enumerate(
-            np.random.choice([False, True], len(grid), p=[1-bias, bias])
-        )
-    }
+    return np.random.choice(len(grid), size=depth, replace=False).tolist()
 
 
 def unique_element_pairs(elements) -> tuple:
@@ -145,9 +143,9 @@ def test_element_adjacency(elements, pair) -> bool:
     :return: bool
         True if elements are adjacent of False if they are not.
     """
-    element_A = elements[pair[0]]
-    element_B = elements[pair[1]]
-    difference = [a - b for a, b in zip(element_A, element_B)]
+    element_a = elements[pair[0]]
+    element_b = elements[pair[1]]
+    difference = [a - b for a, b in zip(element_a, element_b)]
     # elements are adjacent if at lease one of their hex coordinates differ by 1
     if ((np.abs(difference[0]) <= 1) and
             (np.abs(difference[1]) <= 1) and
@@ -163,89 +161,210 @@ def make_hex_network(elements) -> object:
     :param elements: dict
         dictionary containing element ids and hex coordinates
     :return: object
-        retuns a network object
+        returns a network object
     """
     pair_gen = unique_element_pairs(elements)
 
-    G = nx.Graph()
+    graph = nx.Graph()
     for edge in pair_gen:
         if test_element_adjacency(elements, edge):
-            G.add_edge(*edge)
+            graph.add_edge(*edge)
 
-    return G
+    return graph
 
 
-def plot_network(net, active=None) -> object:
+def plot_network(network, active=None) -> object:
     """
     Plot the network with the subset highlighted.
     :param active: dict
         dictionary of active elements, must be same length as elements
-    :param net: object
-        Networkx network object
+    :param network: object
+        NetworkX network object
     :return: object
         The network with updated colours
     """
     # handle colours
-
     if not active:
-        color_map = []
-        color_map = ["green"] * len(net)
+        color_map = ["green"] * len(network)
     else:
         color_map = []
-        for node in net:
+        for node in network:
             if active[node]:
                 color_map.append('blue')
             else:
                 color_map.append('red')
 
-    nx.draw(net, node_color=color_map, with_labels=True)
+    pos = nx.spring_layout(network)
+    nx.draw(network, pos=pos, node_color=color_map, with_labels=True)
     plt.show()
-    return net
+    return network
 
 
-# ToDo plot network with subset
-def get_active_subnetwork(net, active=None) -> object:
+def get_active_sub_network(network, active=None) -> object:
     """
-    Plot the network with the subset highlighted.
+    generates a subnetwork the network with the subset highlighted.
     :param active: dict
         dictionary of active elements, must be same length as elements
-    :param net: object
-        Networkx network object
+    :param network: object
+        NetworkX network object
     :return: object
-        The subnetwork
+        The sub-network
     """
-    if not active:
-        subset_nodes = range(len(net))
+    # if not active:
+    #     subset_nodes = range(len(network))
+    # else:
+    #     subset_nodes = []
+    #     for node in network:
+    #         if active[node]:
+    #             subset_nodes.append(node)
+
+    return network.subgraph(active)
+
+
+def test_network_connectivity(network) -> bool:
+    """
+    Test a network to see if it is fully connected. (wrapper for
+    nx.is_connected())
+    :param network: object
+        NetworkX network object
+    :return: bool
+        Returns True if the network is connected else False
+    """
+    if not network:
+        return False
     else:
-        subset_nodes = []
-        for node in net:
-            if active[node]:
-                subset_nodes.append(node)
-
-    return net.subgraph(subset_nodes)
+        return nx.is_connected(network)
 
 
-# ToDo test the active subset for adjacency.
-# ToDo MCMC to find all active subsets Create Generator.
-# ToDo Find active subsets that are adjacent
+def test_network_isomorphs(network_1, network_2) -> bool:
+    """
+    Test whether two networks are isomorphous. (Wrapper for nx.is_isomorphic())
+    :param network_1: object
+        NetworkX network object
+    :param network_2: object
+        NetworkX network object
+    :return: bool
+        Returns True if two networks are isomorphs else False
+    """
+    if not network_1 or not network_2:
+        return False
+    else:
+        return nx.is_isomorphic(network_1, network_2)
+
+
+def active_subset_markov_random(network, depth=3, starting_node=None) -> list:
+    """
+    From a given starting point create branching dictionary. Kind of a greedy
+    approach, but what you gone do?
+    :param network: object
+        Networkx graph
+    :param starting_node: int, string
+        name of the starting node. Default is None, in which case the most
+        central is chosen
+    :param depth: int
+        The number of active nodes to be returned
+    :return:
+        generates
+    """
+    if not starting_node:
+        starting_node = nx.center(net)[0]
+
+    active_list = [starting_node]
+    current_node = starting_node
+    for element in range(depth-1):
+        adjacent_nodes = [n for n in network.neighbors(current_node)]
+        adjacent_nodes = [x for x in adjacent_nodes if x not in active_list]
+        if not adjacent_nodes:
+            break
+        next_node = np.random.choice(adjacent_nodes, size=1).tolist()[0]
+        active_list.append(next_node)
+        current_node = next_node
+
+    return active_list
+
+
+# TODO compare spatial patterns of isomorphs
+def find_all_isomorphs(network, depth=3, runs=100) -> dict:
+    """
+    Generate a number of active subsets, evaluate whether they exist as
+    isomprphs in the dict.
+    :param network: object
+        Networkx graph
+    :param depth: int
+        The number of active nodes to be returned
+    :param runs: int
+        The number of runs to do.
+    :return: dict
+        Returns a dictionary containing
+    """
+    isomprphs = dict()
+    isomprphs[0] = [active_subset_markov_random(network, depth=depth)]
+    counter = 1
+    for _ in range(runs):
+        flag = 0
+        active_subset = active_subset_markov_random(network, depth=depth)
+        new_subnet = get_active_sub_network(network, active=active_subset)
+        for idx, isomprph in isomprphs.items():
+            target_subnet = get_active_sub_network(network, active=isomprph[0])
+            if test_network_isomorphs(new_subnet, target_subnet):
+                isomprphs[idx].append(active_subset)
+                flag = 1
+        if flag == 0:
+            isomprphs[counter] = [active_subset]
+            counter += 1
+
+    return isomprphs
+
+
+# ToDo find all unique active subsets Create Generator.
 
 
 if __name__ == '__main__':
     print("Start Mesher.py")
+    # np.random.seed(2020)
 
+    # Generate a new hex grid
     myMesh = make_hex_grid(2)
+    # plot_hex(myMesh)
 
-    active = active_subset_random(myMesh, 0.1)
+    # generate an active subset
+    # active1 = active_subset_random(myMesh, 3)
+    # active2 = active_subset_random(myMesh, 3)
 
-    plot_hex(myMesh, active=active)
 
+    # plot hex grid
+    # plot_hex(myMesh, active=active1)
+
+    # create a graph and plot it
     net = make_hex_network(myMesh)
-    plot_network(net, active=active)
+    # plot_network(net, active=active1)
 
-    subnet = get_active_subnetwork(net, active=active)
+    # create a sub-network with only the active elements
+    # subnet1 = get_active_sub_network(net, active=active1)
+    # subnet2 = get_active_sub_network(net, active=active2)
+    # plot_network(subnet1)
 
-    plot_network(subnet)
+    # test that the active set are all connected
+    # print("Is subnet 1 fully connected?", test_network_connectivity(subnet1))
+    # print("Is subnet 2 fully connected?", test_network_connectivity(subnet2))
 
-    print(nx.is_connected(subnet))
+    # print(nx.is_isomorphic(subnet1, subnet1))
+    # print("Are subnet 1 and subnet 2 isomorphs?",
+    #       test_network_isomorphs(subnet1, subnet2))
+
+    active3 = active_subset_markov_random(net, starting_node=None, depth=2)
+    # plot_hex(myMesh, active=active3)
+
+    isos = find_all_isomorphs(net, depth=4, runs=10)
+    print(isos)
+
+    plot_hex(myMesh)
+
+    # subnet1 = get_active_sub_network(net, active=[9,5])
+    # subnet2 = get_active_sub_network(net, active=[9,10])
+    # print("Are subnet 1 and subnet 2 isomorphs?",
+    #       test_network_isomorphs(subnet1, subnet2))
+    # plot_network(subnet1)
+    # plot_network(subnet2)
 
     print("End Mesher.py")
--- README.md
+++ README.md
@@ -7,3 +7,29 @@ Tool for Gareth that will assist in sampling from a hexagonal grid
 * Assign a grid number to each grid point
 * Plot the grid with the grid numbers
 * identify adjacency of elements
+
+## Algorithm
+1. Create a grid in hexagonal coordinate
+    1. Plot the grid
+2. Create a network graph for the grid 
+    1. Plot the network
+3. Create an active subset
+    1. Test node adjacency 
+    2. Plot the active subnet
+4. Network tests
+    1. Test whether network is fully connected
+    2. Test whether two networks are isomprphs
+    3. Test whether two networks have identical shapes
+5. Generate a list of all possible combinations 
+6. Compare many markov subsets and sore the isomorphs in a dictionary
+
+
+
+## combinatorial problem
+of 19 choose 1 = 19
+of 19 choose 2 = 342
+of 19 choose 3 = 5814
+of 19 choose 4 = 93024
+of 19 choose 5 = 1395360
+of 19 choose 6 = 19535040
+    
